96d325f25e3b36cf479b13f4e606f4a6e1c099d6
Esdras Beleza de Noronha <github@contato.esdrasbeleza.com>
2020-07-07 21:50:18 +0100

    Format + remove tslint comment

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
-const port = 8080; // default port to listen
+const app = express()
+const port = 8080 // default port to listen
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+    res.send('Hello world!')
+})
 
 // start the Express server
 app.listen(port, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${port}`);
-});
+    console.log(`server started at http://localhost:${port}`)
+})
